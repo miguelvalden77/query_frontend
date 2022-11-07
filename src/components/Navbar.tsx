6155079bd10de829a8051ef7c5ec13b9df1e14b8
miguelvalden <miguelvalden77@gmail.com>
2022-11-07 02:54:51 +0100

    Logout & Navbar condicional

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,43 @@
 // Paquetes externos
-import {NavLink} from "react-router-dom"
+import {NavLink, useNavigate} from "react-router-dom"
+
+// Hooks
+import { useContext } from "react"
+
+// Context
+import { AuthContext } from "../context/auth.context"
 
 
 const Navbar = ():JSX.Element=>{
 
-    return <nav>
+    const navigate = useNavigate()
+
+    const {isUserActive, authenticateUser} = useContext(AuthContext)
+
+    const handleLogout = ()=>{
+
+        localStorage.removeItem("authToken")
+        authenticateUser()
+        navigate("/")
+
+    }
+
+
+    if(isUserActive){
+        return <nav>
         <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/profile"}>Profile</NavLink>
         <NavLink to={"/allPosts"}>All posts</NavLink>
+        <button onClick={handleLogout}>Logout</button>
+    </nav>
+    } else{
+        return <nav>
+        <NavLink to={"/"}>Home</NavLink>
         <NavLink to={"/login"}>Login</NavLink>
         <NavLink to={"/signup"}>Signup</NavLink>
     </nav>
+    }
+
 
 }
 
