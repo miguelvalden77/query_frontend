620702bfd8a8ac095d098f582735f529e32640a0
miguelvalden <miguelvalden77@gmail.com>
2022-11-20 23:02:26 +0100

    deploy

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -2,7 +2,7 @@
 import axios from "axios"
 
 
-const service = axios.create({baseURL: "http://localhost:5005/api"})
+const service = axios.create({baseURL: "https://query-app-media.herokuapp.com/"})
 
 service.interceptors.request.use((config)=>{
 
